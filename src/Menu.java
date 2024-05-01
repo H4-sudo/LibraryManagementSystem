36be587a1db4839398aa793d576c3064ac10db34
Henrico <h4z3y.bc@gmail.com>
2024-05-01 21:23:07 +0200

    Added a menu class to clean up the 'App.java' implementation.

---
+++ src/Menu.java
@@ -0,0 +1,27 @@
+public class Menu {
+    public void MainMenu() {
+        System.out.println("==========================================================");
+        System.out.println("    ____        _     __   ____              __       ");
+        System.out.println("   / __ \\____ _(_)___/ /  / __ )____  ____  / /_______");
+        System.out.println("  / /_/ / __ `/ / __  /  / __  / __ \\/ __ \\/ //_/ ___/");
+        System.out.println(" / _, _/ /_/ / / /_/ /  / /_/ / /_/ / /_/ / ,< (__  ) ");
+        System.out.println("/_/ |_|\\__,_/_/\\__,_/  /_____/\\____/\\____/_/|_/____/  ");
+        System.out.println("                                                      ");
+        System.out.println("==========================================================");
+        System.out.println("Welcome to the Library Management System! How can we help you today?");
+        System.out.println("1. Add a book");
+        System.out.println("2. Remove a book");
+        System.out.println("3. Add a member");
+        System.out.println("4. Remove a member");
+        System.out.println("5. Search for books");
+        System.out.println("6. Search for members");
+        System.out.println("7. Check out a book");
+        System.out.println("8. Check in a book");
+        System.out.println("9. View all books");
+        System.out.println("10. View all members");
+        System.out.println("11. View all books checked out");
+        System.out.println("0. Exit");
+        System.out.println("==========================================================");
+        System.out.print("Enter your choice: ");
+    }
+}
